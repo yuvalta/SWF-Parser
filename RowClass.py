d45345ef5c52b33af5332a7accf5f586215079dd
yuvalta <tamir.yuval1@gmail.com>
2020-10-29 18:54:37 +0200

    output to new file - formated

--- RowClass.py
+++ RowClass.py
@@ -1,14 +1,32 @@
 class RowClass:
-    def __init__(self, order, user_id, application_id, number_of_nodes, runtime, datetime):
+    def __init__(self, datetime, order, submit_time, runtime, number_of_nodes, user_id, group_id,
+                 application_id, number_of_queues, wait_time=0, average_cpu_time=-1, average_memory_per_node=-1,
+                 requested_processors=-1, requested_runtime=-1, requested_memory=-1, status=-1, number_of_partitions=-1,
+                 preceding_job_number=-1, think_time=-1):
         self.order = order
         self.submit_time = submit_time
         self.wait_time = wait_time
         self.runtime = runtime
-        self.allocated_cpu = allocated_cpu
+        self.number_of_nodes = number_of_nodes
         self.average_cpu_time = average_cpu_time
         self.average_memory_per_node = average_memory_per_node
-
+        self.requested_processors = requested_processors
+        self.requested_runtime = requested_runtime
+        self.requested_memory = requested_memory
+        self.status = status
         self.user_id = user_id
+        self.group_id = group_id
         self.application_id = application_id
-        self.number_of_nodes = number_of_nodes
+        self.number_of_queues = number_of_queues
+        self.number_of_partitions = number_of_partitions
+        self.preceding_job_number = preceding_job_number
+        self.think_time = think_time
         self.datetime = datetime
+
+    def convert_to_string(self):
+        return "{:5s}  {:8s}  {:2s}  {:8s}  {:8s}  {:8s}  {:2s}  {:2s}  {:2s}  {:2s}  {:2s}  {:10s}  {:8s}  {:8s}  {:2s}  {:2s}  {:2s}  {:2s}\n".format(
+            str(self.order), str(self.submit_time), str(self.wait_time), str(self.runtime), str(self.number_of_nodes),
+            str(self.average_cpu_time), str(self.average_memory_per_node), str(self.requested_processors),
+            str(self.requested_runtime), str(self.requested_memory), str(self.status), str(self.user_id),
+            str(self.group_id), str(self.application_id), str(self.number_of_queues), str(self.number_of_partitions),
+            str(self.preceding_job_number), str(self.think_time))
--- main_parser.py
+++ main_parser.py
@@ -1,10 +1,15 @@
 from shutil import copyfile, copy
 import os
-from datetime import datetime
-from RowClass import RowClass  # order, user_id, application_id, number_of_nodes, runtime, datetime
+from datetime import datetime, date
+from RowClass import RowClass
 
-original_log_path = "./NASA-iPSC-1993-0.txt"
-SWF_log = "./SWF_log.txt"
+# datetime, order, submit_time, runtime, number_of_nodes, user_id, group_id,
+# application_id, number_of_queues, wait_time=0, average_cpu_time=-1, average_memory_per_node=-1,
+# requested_processors=-1, requested_runtime=-1, requested_memory=-1, status=-1, number_of_partitions=-1,
+# preceding_job_number=-1, think_time=-1
+
+original_log_path = "log_files/NASA-iPSC-1993-0.txt"
+SWF_log = "log_files/SWF_log.txt"
 
 if os.path.isfile(SWF_log):  # delete SWF file if already exists
     os.remove(SWF_log)
@@ -22,12 +27,31 @@ print(started_time)
 
 with open(SWF_log, "w") as swf_file:
     for row in log_file.readlines():
+
         row_split_list = row.split()
 
         date_and_time = datetime.strptime(row_split_list[4] + " " + row_split_list[5], "%m/%d/%y %H:%M:%S")
-        runtime =
 
-        current_row = RowClass(row_counter, row_split_list[0], row_split_list[1], row_split_list[2], row_split_list[3],
-                               date_and_time)
+        submit_time = datetime.combine(date.today(), date_and_time.time()) - datetime.combine(date.today(),
+                                                                                              started_time.time())  # subtract current time from start time to get time from beginning
+        runtime = row_split_list[3]
+
+        number_of_nodes = row_split_list[2]
+
+        user_id = row_split_list[0]
+
+        if (str.__contains__(user_id, "user")):
+            group_id = 1
+        else:  # sysadmin
+            group_id = 2
+
+        application_id = row_split_list[1]
+
+        number_of_queues = 1
+
+        current_row = RowClass(date_and_time, row_counter, submit_time, runtime, number_of_nodes, user_id, group_id,
+                               application_id, number_of_queues)
+
+        swf_file.write(current_row.convert_to_string())
 
         row_counter += 1
