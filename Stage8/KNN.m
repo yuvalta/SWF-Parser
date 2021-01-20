9763edb7f3d1a7d1cfae543ac2ad2aeec0a3b319
Fahed Nasser <fahednasser@users.noreply.github.com>
2021-01-20 17:42:13 +0200

    KNN - Random

--- Stage8/KNN.m
+++ Stage8/KNN.m
@@ -1,4 +1,7 @@
 MyDataset=ones(69,4);
+RandomJobs=ones(69,4);
+RandomIndices=zeros(69,2);
+
 load('UsersDataframe.mat')
 fn = fieldnames(Runtime);
 for k=1:numel(fn)
@@ -38,11 +41,20 @@ ThinktimesData=load('Thinktimes.mat');
 Data=ones(18239,5);
 fn = fieldnames(RuntimesData);
 lastuserposition=0;
+
+for k=1:numel(fn)
+    RandomIndices(k,1)=ceil((numel(RuntimesData.(fn{k})))*rand);
+    RandomIndices(k,2)=k;
+end
+
 for k=1:numel(fn)
     for i=1:numel(RuntimesData.(fn{k}))
         Data(i+lastuserposition,2)=RuntimesData.(fn{k})(i);
         Data(i+lastuserposition,1)=k;
     end
+    RandomJobs(k,1)=RuntimesData.(fn{k})(RandomIndices(k,1));
+    RandomIndices(k,1)=ceil((numel(RuntimesData.(fn{k})))*rand);
+    RandomIndices(k,2)=k;
     lastuserposition=lastuserposition+i;
 end
 
@@ -52,6 +64,7 @@ for k=1:numel(fn)
     for i=1:numel(InterarrivalstimesData.(fn{k}))
         Data(i+lastuserposition,3)=InterarrivalstimesData.(fn{k})(i);
     end
+        RandomJobs(k,2)=InterarrivalstimesData.(fn{k})(RandomIndices(k,1));
     lastuserposition=lastuserposition+i;
 end
 
@@ -61,6 +74,8 @@ for k=1:numel(fn)
     for i=1:numel(JobSizesData.(fn{k}))
         Data(i+lastuserposition,4)=JobSizesData.(fn{k})(i);
     end
+            RandomJobs(k,3)=JobSizesData.(fn{k})(RandomIndices(k,1));
+
     lastuserposition=lastuserposition+i;
 end
 
@@ -70,8 +85,13 @@ for k=1:numel(fn)
     for i=1:numel(ThinktimesData.(fn{k}))
         Data(i+lastuserposition,5)=ThinktimesData.(fn{k})(i);
     end
+    RandomJobs(k,4)=ThinktimesData.(fn{k})(RandomIndices(k,1));
     lastuserposition=lastuserposition+i;
 end
+
+
+
+    
 ds = array2table(Data);
 ds.Properties.VariableNames{1}='User';
 ds.Properties.VariableNames{2} = 'Runtimes';
@@ -80,9 +100,10 @@ ds.Properties.VariableNames{4} = 'Job_Size';
 ds.Properties.VariableNames{5} = 'Think_Time';
 
 
+X=RandomJobs;
+%X=[ds.Runtimes ds.Interarrival_Time ds.Job_Size ds.Think_Time];
 
- X=[ds.Runtimes ds.Interarrival_Time ds.Job_Size ds.Think_Time];
-idx=knnsearch(X,MyDataset,'K',7);
+idx=knnsearch(X,Data(:,2:5),'K',7);
 for i=1:69
 for j=1:7
     idx(i,j)=Data(idx(i,j),1);
@@ -101,7 +122,7 @@ for k=1:numel(idx(:,1))
 end
 
 Users=sortrows(Users);
-fileID = fopen('TopUsers - 2.txt','w');
+fileID = fopen('TopUsers - 3.txt','w');
 for i=1:7
     fprintf(fileID,'%d \n',Users(69-(i-1),2));
 
