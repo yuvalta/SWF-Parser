625a4acc08c44e680a2db0c1ddf975b927a7901a
JulianSh97 <jshaer02@campus.haifa.ac.il>
2021-01-27 12:43:54 +0200

    Stage 8 Finished

--- Stage9/Validation.py
+++ Stage9/Validation.py
@@ -1,1 +1,15 @@
-
+output1="Output1/output1.txt"
+output2="Output1/output2.txt"
+output3="Output1/output3.txt"
+Log1=[]
+Log2=[]
+Log3=[]
+with open(output1, "r") as output1:
+    for row in output1.readlines():
+        Log1.append(row)
+with open(output2, "r") as output2:
+    for row in output2.readlines():
+        Log2.append(row)
+with open(output3, "r") as output3:
+    for row in output3.readlines():
+        Log3.append(row)
